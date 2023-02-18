dba9746d6f1ed41d38d3f9a18b21822143fc5c55
RadziSzop <radek.krochmall@gmail.com>
2023-02-18 19:43:26 +0100

    Added validation of other routes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # To Do
 
 - [x] Add rate limiter
-- [ ] Fix request validation in other routes
+- [x] Fix request validation in other routes
 - [x] Prevent shortening already shortened link
 - [ ] Make backend stateless (fix geting currentDBIndex using auto increment)
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -4,15 +4,29 @@ import { z } from "zod";
 const destinationUrlRegex =
   /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
 const getUrlScheme = z.object({
-url: 
-z.string({
-  required_error: "Url is required",
-  invalid_type_error: "Url must be a string",
-}).max(8192)
-})
-const getProtectedUrlScheme = z.object({
+  url: z
+    .string({
+      required_error: "Url is required",
+      invalid_type_error: "Url must be a string",
+    })
+    .max(8192),
+});
+const getProtectedUrlSchemeBody = z.object({
+  password: z
+    .string({ invalid_type_error: "Password must be a string. " })
+    .min(6, "Password is too short. ")
+    .max(128, "Password is too long. ")
+    .trim(),
+});
+const getProtectedUrlSchemeParams = z.object({
+  url: z
+    .string({
+      required_error: "Url is required",
+      invalid_type_error: "Url must be a string",
+    })
+    .max(8192),
+});
 
-})
 const postUrlScheme = z.object({
   destinationUrl: z
     .string({
@@ -59,52 +73,49 @@ const postUrlScheme = z.object({
     .optional()
     .or(z.null()),
 });
-type validationType = "postUrl" | "getUrl" | "getProtectedUrl"
-
-// type validationType = "postUrl" | "getUrl";
-export const validate = (type:  validationType): RequestHandler => {
+type validationType = "postUrl" | "getUrl" | "getProtectedUrl";
+export const validate = (type: validationType): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     if (type === "postUrl") {
       const validationOutput = await postUrlScheme.safeParseAsync(req.body);
       if (!validationOutput.success) {
         const validationErrors = validationOutput.error.errors
           .map((element) => {
-            console.log(element);
-
             return element.message;
           })
           .join(" ");
 
         throw new CustomError(validationErrors, 400);
       }
-    }
-    else if (type === "getUrl"){
+    } else if (type === "getUrl") {
       const validationOutput = await getUrlScheme.safeParseAsync(req.params);
       if (!validationOutput.success) {
         const validationErrors = validationOutput.error.errors
           .map((element) => {
-            console.log(element);
-
             return element.message;
           })
           .join(" ");
 
         throw new CustomError(validationErrors, 400);
       }
-    }
-    else if (type === "getProtectedUrl"){
-      const validationOutput = await getUrlScheme.safeParseAsync(req.params);
-      if (!validationOutput.success) {
-        const validationErrors = validationOutput.error.errors
-          .map((element) => {
-            console.log(element);
-
-            return element.message;
-          })
-          .join(" ");
-
-        throw new CustomError(validationErrors, 400);
-      }
+    } else if (type === "getProtectedUrl") {
+      const validationOutput = [
+        await getProtectedUrlSchemeBody.safeParseAsync(req.body),
+        await getProtectedUrlSchemeParams.safeParseAsync(req.params),
+      ];
+      const validationErrors = validationOutput
+        .map((element) => {
+          if (!element.success) {
+            const validationErrors = element.error.errors
+              .map((error) => {
+                return error.message;
+              })
+              .join(" ");
+            return validationErrors;
+          }
+        })
+        .join("");
+      throw new CustomError(validationErrors, 400);
     }
     next();
   };
